32eac291cc4362de64be0a14381507e0fdb6cd21
itsantho <149711510+itsantho@users.noreply.github.com>
2024-11-08 15:06:01 +0100

    Add files via upload
    Ajout d'article + nouveau script pour cacher le signin

--- ScriptMenu.js
+++ ScriptMenu.js
@@ -1,7 +1,13 @@
 /*=============== SHOW MENU ===============*/
+
+
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
+        nav = document.getElementById(navId),
+        iconImage = document.querySelector('.icon'),
+        signInElement = document.querySelector('.signIn');
+
+
 
     toggle.addEventListener('click', () =>{
         // Add show-menu class to nav menu
@@ -9,6 +15,20 @@ const showMenu = (toggleId, navId) =>{
 
         // Add show-icon to show and hide the menu icon
         toggle.classList.toggle('show-icon')
+        // Toggle the visibility of the icon image
+        if (iconImage.style.display === 'none') {
+            iconImage.style.display = 'block';
+        } else {
+            iconImage.style.display = 'none';
+        }
+
+        // Toggle the visibility of the signIn element
+        if (signInElement.style.display === 'none') {
+            signInElement.style.display = 'block';
+        } else {
+            signInElement.style.display = 'none';
+        }
+
     })
 }
 
