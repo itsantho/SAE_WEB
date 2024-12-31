29cfb6b54e524e28a7ad3b700ea41971f75b3414
Paneed <166396881+Paneed@users.noreply.github.com>
2024-12-31 18:34:29 +0100

    Add files via upload

---
+++ SAE_WEB-Acceuil/SAE_WEB-Acceuil/Accueil.js
@@ -0,0 +1,93 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId);
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show-menu'); // Affiche/masque le menu
+            toggle.classList.toggle('show-icon'); // Alterne entre les icônes burger et close
+        });
+
+        // Optionnel : Fermer le menu si on clique ailleurs
+        document.addEventListener('click', (e) => {
+            if (!toggle.contains(e.target) && !nav.contains(e.target)) {
+                nav.classList.remove('show-menu');
+                toggle.classList.remove('show-icon');
+            }
+        });
+    } else {
+        console.error('Toggle or Nav element not found!');
+    }
+};
+
+// Appeler la fonction avec vos IDs
+showMenu('nav-toggle', 'nav-menu');
+
+new Swiper('.card_contenu', {
+    loop: true,
+    spaceBetween:30,
+    dynamicBullets: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints:{
+        0:{
+            slidesPerView:1
+        },
+
+        768:{
+            slidesPerView:3
+        },
+
+        1024:{
+            slidesPerView:4
+        },
+    }
+});
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+function initializeSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        let intervalId = setInterval(nextSlide, 8000);
+    }
+}
+function showSlide(index){
+
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+    slides.forEach(slide =>{
+        slide.classList.remove("displaySlide");
+    })
+    slides[slideIndex].classList.add("displaySlide");
+}
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+function nextSlide(){
+    clearInterval(intervalId);
+    slideIndex++;
+    showSlide(slideIndex);
+}
